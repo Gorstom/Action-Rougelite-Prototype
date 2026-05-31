c8ea9e5f7ca054ad8ccaf6586351f8853b130f8c
Tomasz Gorski <tomikg12345@gmail.com>
2026-05-31 21:06:26 +0200

    high level GDD written

---
+++ docs/IDEAS.md
@@ -0,0 +1,12 @@
+# Milestone 1
+- [x] Player movement
+- [ ] Enemy AI
+- [ ] Basic combat
+
+# Milestone 2
+- [ ] Procedural rooms
+- [ ] Loot
+
+# Milestone 3
+- [ ] Town hub
+- [ ] Merchant
---
+++ scripts/systems/Player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed := 200.0
+
+func _physics_process(delta):
+	var input_dir = Vector2.ZERO
+
+	input_dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+	input_dir = input_dir.normalized()
+
+	velocity = input_dir * speed
+	move_and_slide()
+
+func _ready() -> void:
+	print("PLAYER READY")
