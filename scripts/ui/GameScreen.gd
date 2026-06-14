fee2f24292b2352a5aefc60212140db8a7cff1d4
Tomasz Gorski <tomikg12345@gmail.com>
2026-06-15 00:12:51 +0200

    dungeon room system created

--- scripts/ui/GameScreen.gd
+++ scripts/ui/GameScreen.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var player = $Player
 @onready var health_bar = $CanvasLayer/HealthBar
+@onready var room_manager = $RoomManager
 
 func _ready():
 	health_bar.max_value = player.max_hp
@@ -11,6 +12,14 @@ func _ready():
 	
 	print("MAX HP:", player.max_hp)
 	print("HP:", player.hp)
-		
+	
+	room_manager.room_cleared.connect(_on_room_cleared)
+	room_manager.start_room()
+
+func _on_room_cleared():
+	print("ROOM CLEARED")
+	await get_tree().create_timer(1.0).timeout
+	room_manager.start_room()
+
 func _on_hp_changed(current, max):
 	health_bar.value = current
