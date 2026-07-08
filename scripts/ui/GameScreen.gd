86022cebbb943c426f2fa8fdd7a7cef0f1570fe8
Tomasz Gorski <tomikg12345@gmail.com>
2026-07-08 12:56:35 +0200

    Implemented combat room scene system

--- scripts/ui/GameScreen.gd
+++ scripts/ui/GameScreen.gd
@@ -3,7 +3,6 @@ extends Node2D
 @onready var player = $Player
 @onready var health_bar = $CanvasLayer/HealthBar
 @onready var room_manager = $RoomManager
-@onready var doors = $Doors.get_children()
 
 func _ready():
 	health_bar.max_value = player.max_hp
@@ -14,8 +13,6 @@ func _ready():
 	print("MAX HP:", player.max_hp)
 	print("HP:", player.hp)
 	
-	for door in doors:
-		door.door_entered.connect(_on_door_entered)
 	
 	room_manager.room_cleared.connect(_on_room_cleared)
 	room_manager.start_room()
@@ -29,7 +26,6 @@ func _on_door_entered():
 func _on_room_cleared():
 	print("ROOM CLEARED")
 	room_manager.state = room_manager.RoomState.CLEANED
-	_lock_doors(false)
 	
 	if get_tree() == null:
 		return
@@ -39,13 +35,8 @@ func _on_room_cleared():
 
 func _next_room():
 	room_manager.state = room_manager.RoomState.ACTIVE
-	_lock_doors(true)
 	get_tree().call_group("enemy", "queue_free")
 	room_manager.start_room()
-
-func _lock_doors(value: bool):
-	for door in doors:
-		door.set_locked(value)
 
 
 func _on_hp_changed(current, max):
